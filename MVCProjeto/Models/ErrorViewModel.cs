5ea753feeb37434309735c5047ad7cb8b2df0102
William Marques <willsmarques@gmail.com>
2023-05-02 12:00:47 -0300

    Inicio do Projeto

---
+++ MVCProjeto/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCProjeto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
