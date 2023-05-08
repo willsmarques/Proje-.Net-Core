28415581855de857388817913325894c2f4bf577
William Marques <willsmarques@gmail.com>
2023-05-08 17:20:12 -0300

    Detalhe Vendendo

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -69,6 +69,21 @@ namespace SalesWebMvc.Controllers
         
         }
 
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+
+        }
 
     }
 }
