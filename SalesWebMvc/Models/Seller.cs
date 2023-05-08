df192b3e9d9248b3bcba011956389399085ca596
William Marques <willsmarques@gmail.com>
2023-05-08 11:26:04 -0300

    Tela de Vendedor

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -8,7 +8,7 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string MyProperty { get; set; }
+        public string Email { get; set; }
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Departament Departament { get; set; }
@@ -21,7 +21,7 @@ namespace SalesWebMvc.Models
         {
             Id = id;
             Name = name;
-            MyProperty = myProperty;
+            Email = myProperty;
             BirthDate = birthDate;
             BaseSalary = baseSalary;
             Departament = departament;
