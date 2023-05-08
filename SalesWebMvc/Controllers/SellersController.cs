6ab54a6ce9cce025b387f4ec1f51fd4399b0a31f
William Marques <willsmarques@gmail.com>
2023-05-08 16:57:39 -0300

    Botão Delete Vendedor

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -18,7 +18,7 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult Index()
         {
-            
+
             var list = _sellerService.FindAll();
             return View(list);
 
@@ -26,22 +26,49 @@ namespace SalesWebMvc.Controllers
         }
 
         public IActionResult Create()
-        { 
+        {
             var departaments = _departamentService.FindAll();
             var viewModel = new SellerFormViewModel { Departaments = departaments };
 
             return View(viewModel);
-        
-        
+
+
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Seller seller ) 
-        
-        { 
+        public IActionResult Create(Seller seller)
+
+        {
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id) 
+        { 
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        
+        }
+
+
     }
 }
