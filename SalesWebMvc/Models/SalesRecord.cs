243db25e006b9026ad5263fa763f6b87ff37efe0
William Marques <willsmarques@gmail.com>
2023-05-10 11:40:01 -0300

    Relatorio simple

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -1,23 +1,30 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using SalesWebMvc.Models.Enums;
 namespace SalesWebMvc.Models
 {
     public class SalesRecord
     {
         public int Id { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
-        public SaleStatus MyProperty { get; set; }
+
+        public SaleStatus Status { get; set; }
         public Seller Seller { get; set; }
 
         public SalesRecord() { }
 
-        public SalesRecord(int id, DateTime date, double amount, SaleStatus myProperty, Seller seller)
+        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
         {
             Id = id;
             Date = date;
             Amount = amount;
-            MyProperty = myProperty;
+            Status = status;
             Seller = seller;
         }
     }
